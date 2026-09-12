33f5890f4c6156deef87388453cc1fa782bf79b3
ArtemKo7v <artemko7v@gmail.com>
2026-09-13 00:38:21 +0200

    Bug with inputs fixed

--- web/js/dynamic_selectors.js
+++ web/js/dynamic_selectors.js
@@ -29,16 +29,21 @@ function normalizePairs(n) {
     is = entries(n, "image_"); ts = entries(n, "text_");
     while (is.length > 1 && is.at(-1).input.link == null && ts.at(-1).input.link == null && is.at(-2).input.link == null && ts.at(-2).input.link == null) { n.removeInput(n.inputs.indexOf(ts.at(-1).input)); n.removeInput(n.inputs.indexOf(is.at(-1).input)); is = entries(n, "image_"); ts = entries(n, "text_"); }
 }
+function removeInputs(n, prefixes) {
+    for (let index = (n.inputs?.length ?? 0) - 1; index >= 0; index -= 1) {
+        if (prefixes.some(prefix => new RegExp(`^${prefix}\\d+$`).test(n.inputs[index].name ?? ""))) n.removeInput(index);
+    }
+}
 function restore(n, data, prefixes, create) {
     const indexes = (data?.inputs ?? []).flatMap(x => prefixes.map(p => Number(new RegExp(`^${p}(\\d+)$`).exec(x.name ?? "")?.[1]))).filter(Number.isInteger);
     for (let i = 1; i <= Math.min(Math.max(1, ...indexes), MAX); i++) create(n, i);
 }
-function install(n, normalize, restoreInputs) {
+function install(n, normalize, restoreInputs, reset) {
     const configured = n.onConfigure; n.onConfigure = function(data) { restoreInputs(this, data); configured?.apply(this, arguments); restoreInputs(this, data); normalize(this); };
     const changed = n.onConnectionsChange; n.onConnectionsChange = function() { changed?.apply(this, arguments); normalize(this); dirty(this); };
-    normalize(n);
+    reset?.(n); normalize(n);
 }
 app.registerExtension({ name: "ArtemKo7v.UsefulStuffNodes.DynamicSelectors", nodeCreated(n) {
-    if (n.constructor.type === ANY) install(n, normalizeAny, (x,d) => restore(x,d,["any_"], (node,i) => add(node, `any_${i}`, "*")));
-    if (n.constructor.type === PAIRS) install(n, normalizePairs, (x,d) => restore(x,d,["image_", "text_"], addPair));
+    if (n.constructor.type === ANY) install(n, normalizeAny, (x,d) => restore(x,d,["any_"], (node,i) => add(node, `any_${i}`, "*")), node => removeInputs(node, ["any_"]));
+    if (n.constructor.type === PAIRS) install(n, normalizePairs, (x,d) => restore(x,d,["image_", "text_"], addPair), node => removeInputs(node, ["image_", "text_"]));
 }});
